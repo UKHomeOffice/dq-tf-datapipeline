3adef9939a86916435903bb9360d391f68903da9
david-wirelab <info@wirelab.co.uk>
2017-11-30 16:47:17 +0000

    initial commit

---
+++ instance.tf
@@ -0,0 +1,122 @@
+variable "appsvpc_id" {}
+variable "opsvpc_cidr_block" {}
+variable "appsvpc_cidr_block" {}
+variable "data_pipe_apps_cidr_block" {}
+variable "az" {}
+variable "name_prefix" {}
+
+
+locals {
+  name_prefix = "${var.name_prefix}apps-"
+}
+
+resource "aws_subnet" "data_pipe_apps" {
+  vpc_id                  = "${var.appsvpc_id}"
+  cidr_block              = "${var.data_pipe_apps_cidr_block}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${var.az}"
+
+  tags {
+    Name = "${local.name_prefix}data-pipeline-subnet"
+  }
+}
+
+variable instance_type {
+  default = "t2.nano"
+}
+
+data "aws_ami" "linux_connectivity_tester" {
+  most_recent = true
+
+  filter {
+    name = "name"
+
+    values = [
+      "connectivity-tester-linux*",
+    ]
+  }
+
+  owners = [
+    "093401982388",
+  ]
+}
+
+resource "aws_instance" "dp_postgres" {
+  ami                    = "${data.aws_ami.linux_connectivity_tester.id}"
+  instance_type          = "${var.instance_type}"
+  subnet_id              = "${aws_subnet.data_pipe_apps.id}"
+  vpc_security_group_ids = ["${aws_security_group.bastions_db.id}"]
+
+  tags {
+    Name = "${local.name_prefix}data-pipeline-postgres"
+  }
+
+  user_data = "CHECK_self=127.0.0.1:8080 CHECK_google=google.com:80 CHECK_googletls=google.com:443 LISTEN_db=0.0.0.0:1433 LISTEN_rdp=0.0.0.0:3389"
+}
+
+resource "aws_instance" "dp_web" {
+  ami                    = "${data.aws_ami.linux_connectivity_tester.id}"
+  instance_type          = "${var.instance_type}"
+  subnet_id              = "${aws_subnet.data_pipe_apps.id}"
+  vpc_security_group_ids = ["${aws_security_group.bastions_web.id}"]
+
+  tags {
+    Name = "${local.name_prefix}data-pipeline-web"
+  }
+
+  user_data = "CHECK_self=127.0.0.1:8080 CHECK_google=google.com:80 CHECK_googletls=google.com:443 LISTEN_rdp=0.0.0.0:3389"
+}
+
+resource "aws_security_group" "bastions_db" {
+  vpc_id = "${var.appsvpc_id}"
+
+  tags {
+    Name = "${local.name_prefix}data-pipeline-db-sg"
+  }
+
+  ingress {
+    from_port   = 1433
+    to_port     = 1433
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${var.appsvpc_cidr_block}",
+      "${var.opsvpc_cidr_block}"
+    ]
+  }
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["${var.opsvpc_cidr_block}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "bastions_web" {
+  vpc_id = "${var.appsvpc_id}"
+
+  tags {
+    Name = "${local.name_prefix}data-pipeline-web-sg"
+  }
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["${var.opsvpc_cidr_block}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ tests/dp_test.py
@@ -0,0 +1,58 @@
+# pylint: disable=missing-docstring, line-too-long, protected-access
+import unittest
+from runner import Runner
+
+
+class TestE2E(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.snippet = """
+
+            provider "aws" {
+              region = "eu-west-2"
+              skip_credentials_validation = true
+              skip_get_ec2_platforms = true
+            }
+
+            module "data_pipeline" {
+              source = "./mymodule"
+
+              providers = {
+                aws = "aws"
+              }
+
+              appsvpc_id                  = "1234"
+              appsvpc_cidr_block          = "1.2.3.0/24"
+              opsvpc_cidr_block           = "1.2.3.0/24"
+              data_pipe_apps_cidr_block   = "10.1.8.0/24"
+              az                          = "eu-west-2a"
+              name_prefix                 = "dq-"
+            }
+        """
+        self.result = Runner(self.snippet).result
+
+    def test_root_destroy(self):
+        self.assertEqual(self.result["destroy"], False)
+
+    def test_data_pipe_apps(self):
+        self.assertEqual(self.result['data_pipeline']["aws_subnet.data_pipe_apps"]["cidr_block"], "10.1.8.0/24")
+
+    def test_name_prefix_data_pipe_apps_subnet(self):
+        self.assertEqual(self.result['data_pipeline']["aws_subnet.data_pipe_apps"]["tags.Name"], "dq-apps-data-pipeline-subnet")
+
+    def test_name_prefix_dp_postgres(self):
+        self.assertEqual(self.result['data_pipeline']["aws_instance.dp_postgres"]["tags.Name"], "dq-apps-data-pipeline-postgres")
+
+    def test_name_prefix_dp_web(self):
+        self.assertEqual(self.result['data_pipeline']["aws_instance.dp_web"]["tags.Name"], "dq-apps-data-pipeline-web")
+
+    def test_name_prefix_bastions_db(self):
+        self.assertEqual(self.result['data_pipeline']["aws_security_group.bastions_db"]["tags.Name"], "dq-apps-data-pipeline-db-sg")
+
+    def test_name_prefix_bastions_web(self):
+        self.assertEqual(self.result['data_pipeline']["aws_security_group.bastions_web"]["tags.Name"], "dq-apps-data-pipeline-web-sg")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
