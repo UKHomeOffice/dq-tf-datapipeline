9666521848c34a57bfd6c33967800607cd851c89
david-wirelab <info@wirelab.co.uk>
2017-12-05 13:10:37 +0000

    fix fmt

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,23 @@
 output "appsvpc_id" {
   value = "${var.appsvpc_id}"
 }
+
 output "opssubnet_cidr_block" {
   value = "${var.opssubnet_cidr_block}"
 }
+
 output "appsvpc_cidr_block" {
   value = "${var.appsvpc_cidr_block}"
 }
+
 output "data_pipe_apps_cidr_block" {
   value = "${var.data_pipe_apps_cidr_block}"
 }
+
 output "az" {
   value = "${var.az}"
 }
+
 output "name_prefix" {
   value = "${var.name_prefix}"
 }
