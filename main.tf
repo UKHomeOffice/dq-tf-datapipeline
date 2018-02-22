c5e6095b324864644e334c02a1cabad9ea7a4643
david-wirelab <info@wirelab.co.uk>
2018-02-22 13:45:13 +0000

    remove ignore flags

--- main.tf
+++ main.tf
@@ -30,12 +30,6 @@ resource "aws_instance" "dp_web" {
 
   lifecycle {
     prevent_destroy = true
-
-    ignore_changes = [
-      "user_data",
-      "ami_name",
-      "instance_type",
-    ]
   }
 
   tags = {
